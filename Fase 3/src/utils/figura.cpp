1980b68bb013e17ca52a4afe7b06b9e768279699
MartaRodrigues03 <martarodrigues2003@gmail.com>
2024-04-20 23:12:30 +0100

    inicio sistema solar animado

---
+++ Fase 3/src/utils/figura.cpp
@@ -0,0 +1,34 @@
+#include "figura.hpp"
+using namespace std;
+
+//figura � constituida por pontos
+
+
+void addPonto(Figura& f, Ponto p) {
+	f.pontos.push_back(p);
+}
+
+void writeToFile(Figura f, const char* file_path) {
+	
+	if (f.pontos.empty()) {
+		std::cout << "Figure is Empty!\n";
+		return;
+	}
+	
+	std::ofstream file(file_path);
+
+	if (!file) {
+		std::cout << "Error opening file to write.\n";
+		return;
+	}
+
+	int tamanho = f.pontos.size();
+	file << tamanho << std::endl;
+
+	for (const auto& ponto : f.pontos){
+		file << getX(ponto) << ";" << getY(ponto) << ";" << getZ(ponto) << "\n";
+	}
+
+	file.close();
+	std::cout << "Writing to file done!\n";
+}
