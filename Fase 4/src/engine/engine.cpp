d53d80f8e2802276eeceb403daa682a42baafde6
ema-12-martins <a97678@alunos.uminho.pt>
2024-05-26 10:36:59 +0100

    mais direito

--- Fase 4/src/engine/engine.cpp
+++ Fase 4/src/engine/engine.cpp
@@ -52,10 +52,6 @@ float beta2 = M_PI / 4; //beta is anbiguos in std beta
 float raio = 5.0f;
 GLenum mode = GL_LINE;
 
-
-std::vector<float> vertexB;
-std::vector<float> vertexN;
-
 //Iluminação
 enum LightType{
     POINT,
@@ -332,7 +328,8 @@ void drawFigure(std::string figureFile){
 
     if (vertices < std::stoi(linha)) vertices = std::stoi(linha);
 
-    
+    std::vector<float> vertexB;
+    std::vector<float> vertexN;
     std::vector<float> vertexT;
 
     // Leia o arquivo linha por linha
@@ -359,21 +356,13 @@ void drawFigure(std::string figureFile){
                     vertexN.push_back(value);
                 }
                 else if (tokenCount % 3 == 2){
-                    //Aqui as coordenadas das texturasa
+                    //Aqui as coordenadas das texturas
                     vertexT.push_back(value);
                 }
             }
             tokenCount++;
         }
-        //for (int i = 0; i < vertexN.size(); i++) {
-        //    std::cout<< vertexN[i] << std::endl;
-        //    vertexN[i] += vertexB[i];
-        //    std::cout<< vertexN[i] << std::endl;
-        //} 
     }
-
-
-    
 
     // Feche o file
     file.close();
@@ -538,25 +527,6 @@ void renderScene(void) {
     //............................................
 
     drawAxis();
-
-    //glBegin(GL_LINES);
-    //glColor3f(1,0,0);
-    //glVertex3f(1,1,1);
-    //for (size_t i = 0; i < vertexN.size(); i += 3) {
-        //std::cout << vertexN[i] << " "; // Imprime o valor atual do vetor
-    //    glVertex3f((vertexN[i]) + vertexB[i], vertexN[i+1] + vertexB[i+1], vertexN[i+2] + vertexB[i+2]);
-    //}
-    //glEnd();
-    glBegin(GL_LINES);
-    glColor3f(1, 0, 0); // Set color for normal vectors
-    for (size_t i = 0; i < vertexN.size(); i += 3) {
-        glVertex3f(vertexN[i], vertexN[i + 1], vertexN[i + 2]);
-        //glVertex3f(vertexB[i] + vertexN[i], vertexB[i + 1] + vertexN[i + 1], vertexB[i + 2] + vertexN[i + 2]);
-    }
-    glColor3f(1, 0, 0); // Set color for normal vectors
-    glEnd();
-
-
 
     glPolygonMode(GL_FRONT_AND_BACK, mode);
 
@@ -815,14 +785,14 @@ int main(int argc, char *argv[]) {
     glEnable(GL_LIGHT0);
     glEnable(GL_DEPTH_TEST);
     glEnable(GL_CULL_FACE);
+    glEnable(GL_RESCALE_NORMAL);
+    
     // Enable smooth shading
     glShadeModel(GL_SMOOTH);
 
     // Initialize VBOs
     glGenBuffers(numFigurasMax*2, buffers);
     processVBOs(mainGroup);
-
-    
 
     // Enter GLUT's main cycle
     glutMainLoop();
